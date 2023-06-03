9a6fd2bccf72051e5f984887bcc10a22c7790da6
Carola <user@users-MacBook-Pro.local>
2023-06-03 00:49:05 -0400

    desafio terminado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+5.times do
+Character.create(name: Faker::Movies::HarryPotter.character, location: Faker::Movies::HarryPotter.location, house: Faker::Movies::HarryPotter.house )
+end
